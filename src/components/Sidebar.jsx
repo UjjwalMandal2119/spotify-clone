0c2165eaf396fa4a8244574837d0de7c2bd015b2
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-01 23:15:35 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from '../spotify-assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+     <div className='flex items-center gap-3 pl-8 cursor-pointer'></div>
+      <img src={}  > </img>
+    </div>
+  )
+}
+
+export default Sidebar
