0035c8154c62e8910b72d29ad71c7483f9e4dab5
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-01 23:37:34 +0530

    just doing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,10 +3,17 @@ import {assets} from '../spotify-assets/assets'
 
 const Sidebar = () => {
   return (
-    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-     <div className='flex items-center gap-3 pl-8 cursor-pointer'></div>
-      <img src={}  > </img>
-    </div>
+   <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+   <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
+   <div className='flex items-center gap-3 pl-8 cursor-pointer '>
+   <img className='w-6' src='assets.home_icon'></img>
+   <p className='font-bold'>Home</p>
+
+   </div>
+
+   </div>
+
+   </div>
   )
 }
 
