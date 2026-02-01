52b11e09286ea545d3cebf8c964869247cb88e60
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-01 23:50:45 +0530

    doing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,11 @@ const Sidebar = () => {
    <p className='font-bold'>Home</p>
 
    </div>
+   <div className='flex items-center gap-3 pl-8 cursor-pointer '>
+   <img className='w-6' src='assets.search_icon'></img>
+   <p className='font-bold'>Search</p>
+
+   </div>
 
    </div>
 
